ea3c48cd95ef247579e11db366a7b5114be5249d
Don4ik89 <d.kair.89@gmail.com>
2023-04-01 00:03:31 +0600

    base v1

---
+++ src/main/java/kz/kair/controller/HomeController.java
@@ -0,0 +1,12 @@
+package kz.kair.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Helloo Test";
+    }
+}
